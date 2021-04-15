febff4b4abb11104c959a61ebf44dabb4d8f5c89
wjdwngml1001 <67431553+wjdwngml1001@users.noreply.github.com>
2021-04-15 15:51:31 +0900

    Update 2020105660.cpp

--- 2020105660.cpp
+++ 2020105660.cpp
@@ -2,8 +2,8 @@
 using namespace std;
 
 int main() {
-	for (int i = 1; i < 10; i++)
-		for (int j = 1; j < 10; j++)
+	for (int i = 1; i < 9; i++)
+		for (int j = 1; j < 9; j++)
 			cout << i << " x " << j << " = " << i * j << endl;
 	return 0;
 }
